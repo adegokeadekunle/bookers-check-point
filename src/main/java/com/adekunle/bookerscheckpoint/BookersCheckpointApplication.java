eab04d8f1d8f0e64b321eebf2b94eda55ca9f9cb
adegokeadekunle2 <adegokeadekunle2@gmail.com>
2022-06-06 00:44:48 +0100

    Bookers Check Point App

---
+++ src/main/java/com/adekunle/bookerscheckpoint/BookersCheckpointApplication.java
@@ -0,0 +1,13 @@
+package com.adekunle.bookerscheckpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookersCheckpointApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookersCheckpointApplication.class, args);
+    }
+
+}
